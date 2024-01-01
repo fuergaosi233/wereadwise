2c00ae8b7b66156126f39b518b6c4a3369e7076c
zephrxx <tom_808@hotmail.com>
2023-12-31 20:41:58 -0800

    Fix issue: "Error: Cannot read properties of undefined (reading 'title')"

--- source/lib/index.ts
+++ source/lib/index.ts
@@ -57,7 +57,7 @@ const getAllInfos = async (): Promise<WEreadDataInfo> => {
   console.log(value);
   if (!value) {
     throw new Error('Get info failed');
-  }
+    }
 
   return JSON.parse(value);
 };
@@ -120,7 +120,7 @@ export const SyncAllData = async ({
 }): Promise<void> => {
   setStage('Try to get all infos from weread');
   const value = await getAllInfos();
-  const bookIds = value.shelf.shelfIndexes
+    const bookIds = value.shelf.shelfIndexes
     .map((book) => book.bookId)
     .concat(value.shelf.archive.map((archive) => archive.bookIds).flat());
   setBookCount(bookIds.length);
@@ -131,7 +131,8 @@ export const SyncAllData = async ({
   for (const bookId of bookIds) {
     const bookMarkInfo = await getAllBookMark(bookId, 1);
     const { updated, book } = bookMarkInfo;
-    const hightlights = updated.map(
+    try {
+      const hightlights = updated.map(
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       (item: { markText: any; range: string; createTime: number }) => {
         return {
@@ -145,14 +146,18 @@ export const SyncAllData = async ({
           location: Number(item.range.split('-')[0]),
           location_type: 'order',
           highlighted_at: new Date(item.createTime * 1000).toISOString(),
-        };
-      }
-    );
-    allGetCount += 1;
-    setStage(
-      `Try to get all bookmarks from weread, ${book.title} ${allGetCount}/${bookIds.length}`
-    );
-    allHighlights = allHighlights.concat(hightlights);
+          };
+        }
+      );
+      allGetCount += 1;
+      setStage(
+        `Try to get all bookmarks from weread, ${book.title} ${allGetCount}/${bookIds.length}`
+      );
+      allHighlights = allHighlights.concat(hightlights);
+    } catch (error) {
+      console.log(`Error when reading book: ${bookId}`);
+      console.log(error);
+    }
   }
   console.log(`allHighlights.length: ${allHighlights.length}`);
   console.log(allHighlights);
